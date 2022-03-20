6ff37f2394184f3d2d4b0393d5852c25dfeb4922
Михаил Сонин <mishasonin@mail.ru>
2022-03-20 22:36:39 +0300

    fix settings

--- hammersystems/hammersystems/settings.py
+++ hammersystems/hammersystems/settings.py
@@ -133,8 +133,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
